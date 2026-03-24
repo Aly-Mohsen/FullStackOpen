ca144d0b0ad3368bbc35f6153ad334209d24e7b8
Aly-Mohsen <aly.mohsen1000@gmail.com>
2026-03-24 05:17:41 +0400

    Exercise 2.5

---
+++ Part2/courseinfo-part2/src/components/Course.jsx
@@ -0,0 +1,33 @@
+const Header = ({ name }) => {
+  return (
+    <h2>{name}</h2>
+  )
+}
+const Part = ({ part }) => {
+  return (
+    <li>{part.name} {part.exercises}</li>
+  )
+}
+const Content = ({ parts }) => {
+  return (
+    <ul>
+      {parts.map(part => <Part key={part.id} part={part} />)}
+    </ul>
+  )
+}
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <p><strong>Total of {total} exercises</strong></p>
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+export default Course
