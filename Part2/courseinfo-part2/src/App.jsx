ea81748be1304cbb09fd8ce9df796045b47737ce
Aly-Mohsen <aly.mohsen1000@gmail.com>
2026-03-24 05:12:33 +0400

    Exercise 2.5 Refactor course components into separate Course.jsx file

--- Part2/courseinfo-part2/src/App.jsx
+++ Part2/courseinfo-part2/src/App.jsx
@@ -1,37 +1,5 @@
+import Course from './components/Course'
 const PageHeader = () => <h1>Web development curriculum</h1>
-const Header = ({ name }) => {
-  return (
-    <h2>{name}</h2>
-  )
-}
-const Part = ({ part }) => {
-  return (
-    <li>{part.name} {part.exercises}</li>
-  )
-}
-const Content = ({ parts }) => {
-  return (
-    <ul>
-      {parts.map(part => <Part key={part.id} part={part} />)}
-    </ul>
-  )
-}
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-  return <p><strong>Total of {total} exercises</strong></p>
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
-
 const App = () => {
   const courses = [
     {
